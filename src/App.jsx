f9550f2a9c3034d65146258c7887c9330729e7ce
Tej Patel <tejp@webmobtech.com>
2023-03-02 16:15:42 +0530

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
-import './App.css'
+import React, { useState, useEffect, memo, useMemo } from "react";
+import "./App.css";
+
+let Memos = () => {
+  console.log("Memmmmmooooooo");
+  return <div style={{ width: 40, height: 20, backgroundColor: "blue" }} />;
+};
+
+const Memod = memo(Memos);
+
+let handleUseMemos = () => {
+  console.log("Initial Calls");
+}
 
 function App() {
+  const [render, setRender] = useState(0);
+  const [cube, setCube] = useState(false);
 
+
+
+  let handleRender = () => {
+    console.log("Rendered", render);
+    setRender((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    handleRender;
+  });
+
+  let cubeColor = () => {
+    console.log("color changed");
+    setCube(!cube);
+  };
 
   return (
     <div className="App">
-     <p>Hello</p>
+      <button onClick={handleRender}>Re-Render</button>
+      <br />
+      <br />
+      {render}
+      <br />
+      <br />
+      <div className={cube ? "cube" : "cubeChange"}></div>
+      <br />
+      <button onClick={handleUseMemos}>Click Use MEmos</button>
+      <button onClick={cubeColor}>Change Cube Color</button>
+      <div>
+        <Memod />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
